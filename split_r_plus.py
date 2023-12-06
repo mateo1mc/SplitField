c2c1da22223bdf077c668537b0f6fdd5d3578ab6
MATEO CELA <112934949+mateo1mc@users.noreply.github.com>
2023-12-06 12:26:25 +0100

    Update and rename final_r_plus.py to split_r_plus.py

--- split_r_plus.py
+++ split_r_plus.py
@@ -1,7 +1,17 @@
+import os
 import csv
 
+# Get the directory of the currently executing script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# File name
+file_name = 'file.csv'
+
+# Construct the full path to the CSV file
+file_path = os.path.join(script_dir, file_name)
+
 # Open the CSV file for reading and writing
-with open('file.csv', 'r+', newline='', encoding='utf8') as csvfile:
+with open(file_path, 'r+', newline='', encoding='utf8') as csvfile:
     # Create a CSV reader object
     reader = csv.DictReader(csvfile)
 
@@ -17,23 +27,29 @@ with open('file.csv', 'r+', newline='', encoding='utf8') as csvfile:
         for row in reader:
             # Split the "MainFeatures" column into separate values
             values = row['MainFeatures'].split('|')
+
             if len(values) > 1:
                 # Create new column names based on the number of values
                 new_column_names = [f'bullet{i+1}' for i in range(len(values))]
+
             else:
                 # If there's only one value, create a single new column named "bullet1"
                 new_column_names = ['bullet1']
+
             # Add the new column names to the fieldnames list if they're not already present
             for name in new_column_names:
                 if name not in fieldnames:
                     fieldnames.append(name)
+
             # Update the row with the new column values
             row.update(dict(zip(new_column_names, values)))
+
             # Append the updated row to the list
             updated_rows.append(row)
 
         # Truncate the file to the start position
         csvfile.seek(0)
+
         # Create a CSV writer object
         writer = csv.DictWriter(csvfile, fieldnames)
 
@@ -45,3 +61,6 @@ with open('file.csv', 'r+', newline='', encoding='utf8') as csvfile:
 
         # Truncate the file to the current position
         csvfile.truncate()
+
+        # Print message after finish it
+        print("Data Updated Successfully")
